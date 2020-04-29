7529ae5a40ff8d853d86c0139591086e6a21a7ad
daniel guindel <d.guindel.gonzalez@hotmail.com>
2020-04-29 13:31:23 +0200

    change masks done!

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CanvasManager : MonoBehaviour
+{
+    #region Public Properties
+
+    public GameObject IronManGO;
+    public GameObject DefaultFaceGO;
+
+    #endregion
+
+    #region Private Fields
+
+    private ARFaceManager arFaceManager;
+    private TextMeshProUGUI textMeshPro;
+    //private GameObject selectedMask;
+    
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arFaceManager = GameObject.Find("AR Session Origin").GetComponent<ARFaceManager>();
+        textMeshPro = GameObject.Find("Text").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IronManFaceSelect()
+    {
+        arFaceManager.facePrefab = IronManGO;
+        textMeshPro.text = "Iron Man";
+        ReloadComponents();
+    }
+
+    public void DefaultFaceSelect()
+    {
+        arFaceManager.facePrefab = DefaultFaceGO;
+        textMeshPro.text = "Default Face";
+        ReloadComponents();
+    }
+
+    void ReloadComponents()
+    {
+        GameObject arSessionGO = GameObject.Find("AR Session");
+        GameObject arSessionOriginGO = GameObject.Find("AR Session Origin");
+        arSessionGO.SetActive(false);
+        arSessionOriginGO.SetActive(false);
+        arSessionGO.SetActive(true);
+        arSessionOriginGO.SetActive(true);
+    }
+}
